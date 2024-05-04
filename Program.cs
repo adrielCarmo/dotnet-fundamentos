b9f18924b270d1625da0f2d1163df972cc2d22a4
adrielCarmo <adrieldovale7@gmail.com>
2024-05-04 17:35:45 -0400

    desafio finalizado

--- Program.cs
+++ Program.cs
@@ -1,2 +1,52 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using dotnet_desafio_fundamentos.Models;
+
+// Coloca o encoding para UTF8 para exibir acentuação
+Console.OutputEncoding = System.Text.Encoding.UTF8;
+
+decimal precoInicial = 0;
+decimal precoPorHora = 0;
+int opcao;
+
+Console.WriteLine("Seja bem vindo ao sistema de estacionamento!\nDigite o preço inicial:");
+precoInicial = Convert.ToDecimal(Console.ReadLine());
+
+Console.WriteLine("Agora digite o preço por hora:");
+precoPorHora = Convert.ToDecimal(Console.ReadLine());
+
+// Instancia a classe Estacionamento, já com os valores obtidos anteriormente
+Estacionamento estacionamento = new Estacionamento(precoInicial, precoPorHora);
+
+do 
+{
+    Console.Clear();
+    Console.WriteLine("Digite a sua opção:");
+    Console.WriteLine("1 - Cadastrar veículo");
+    Console.WriteLine("2 - Remover veículo");
+    Console.WriteLine("3 - Listar veículos");
+    Console.WriteLine("4 - Encerrar");
+    
+    opcao = Convert.ToInt32(Console.ReadLine());
+
+    switch (opcao) {
+        case 1:
+            estacionamento.AdicionarVeiculo();
+            break;
+        case 2:
+            estacionamento.RemoverVeiculo();
+            break;
+        case 3:
+            estacionamento.ListarVeiculos();
+            break;
+        case 4:
+            Console.WriteLine("ENCERRANDO PROGRAMA...");
+            Environment.Exit(1);
+            break;
+        default:
+            Console.WriteLine("Opção inválida");
+            break;
+    }
+
+    Console.WriteLine("Pressione uma tecla para continuar");
+    Console.ReadLine();
+
+} while (opcao != 4);
--- obj/Debug/net8.0/dotnet-desafio-fundamentos.AssemblyInfo.cs
+++ obj/Debug/net8.0/dotnet-desafio-fundamentos.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("dotnet-desafio-fundamentos")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+701aa9ff553bccb510e8a2b79662fd292ec2afa2")]
 [assembly: System.Reflection.AssemblyProductAttribute("dotnet-desafio-fundamentos")]
 [assembly: System.Reflection.AssemblyTitleAttribute("dotnet-desafio-fundamentos")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
